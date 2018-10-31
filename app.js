c15f279735c125053dd7558e75457dd01d79c4d7
Victoria Murray <victoria.rose.murray@gmail.com>
2018-10-31 12:04:40 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+
 
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
+
 app.listen(3000, () => {
-  console.log('App listening on port 3000!')
+    console.log('App listening on port 3000!')
 })
+
+// OUR MOCK ARRAY OF PROJECTS
+// let reviews = [{
+//         title: "Bad Review",
+//         movieTitle: "Films"
+//     },
+//     {
+//         title: "Okay Movie",
+//         movieTitle: "Titanic"
+//     }
+// ]
+
+const Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String
+});
