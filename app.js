afa53ec9ee23d5540e0e981a911d45467a8b1310
Victoria Murray <victoria.rose.murray@gmail.com>
2018-10-31 12:29:02 -0700

    users can see single reviews

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
 })
 
+app.get('/reviews/:id', (req, res) => {
+  res.send('I\'m a review')
+});
+
 app.post('/reviews', (req, res) => {
   console.log(req.body);
   // res.render('reviews-new', {});
@@ -40,6 +44,22 @@ app.post('/reviews', (req, res) => {
   })
 })
 
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
