e17c09fc971fd994cc1bc1e45f576a82c3399204
Victoria Murray <victoria.rose.murray@gmail.com>
2018-11-03 13:52:11 -0700

    users can delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -13,4 +13,14 @@ module.exports = (app) => {
         });
     });
 
+    // DELETE
+    app.delete('/reviews/comments/:id', function (req, res) {
+        console.log("DELETE comment")
+        Comment.findByIdAndRemove(req.params.id).then((comment) => {
+            res.redirect(`/reviews/${comment.reviewId}`);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
+
 }
